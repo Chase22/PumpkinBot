60bd307fe03562f21b57b493fcb17458a74bace2
chase <chase@mailbox.org>
2019-10-19 17:11:11 +0200

    Use blocking IO

--- src/main/java/io/github/chase22/telegram/pumpkinbot/Portbinder.java
+++ src/main/java/io/github/chase22/telegram/pumpkinbot/Portbinder.java
@@ -1,15 +1,17 @@
 package io.github.chase22.telegram.pumpkinbot;
 
 import io.undertow.Undertow;
-import io.undertow.util.StatusCodes;
+import io.undertow.server.handlers.BlockingHandler;
 
 public class Portbinder {
 
     Portbinder(final int port) {
+        BlockingHandler blockingHandler = new BlockingHandler(exchange ->
+                exchange.getResponseSender().send("OK"));
+
             Undertow undertow = Undertow.builder()
                     .addHttpListener(port, "localhost")
-                    .setHandler(exchange ->
-                            exchange.getResponseSender().send("OK")).build();
+                    .setHandler(blockingHandler).build();
 
             undertow.start();
         }
